8eb46ced3921e574d252c2a99de374c645d54d95
CHEN ZHANG <czha9486@uni.sydney.edu.au>
2019-10-23 07:25:41 +1100

    Insert Data function all done.

--- Kinder-Animal-Food/src/main/java/au/usyd/elec5619/KAF/service/StoreServiceImpl.java
+++ Kinder-Animal-Food/src/main/java/au/usyd/elec5619/KAF/service/StoreServiceImpl.java
@@ -35,11 +35,11 @@ public class StoreServiceImpl implements StoreService {
 	public Store searchStore(Store store) {
 		return storeDao.searchStore(store).get(0);
 	}
-	
+
 	@Override
 	@Transactional
 	public List<Store> storeList() {
-		
+
 		return storeDao.storeList();
 	}
 
@@ -92,7 +92,7 @@ public class StoreServiceImpl implements StoreService {
 	@Override
 	@Transactional
 	public boolean setBrandsNum(Integer store_id, Integer brands_num) {
-		
+
 		if (storeDao.setBrandsNum(store_id, brands_num)) {
 			return true;
 		} else {
@@ -102,43 +102,44 @@ public class StoreServiceImpl implements StoreService {
 
 	@Override
 	@Transactional
-	public List<Store> searchStoreByDistance(String[] userCoordinates, List<Integer> storeId,int searchRadius) {
+	public List<Store> searchStoreByDistance(String[] userCoordinates, List<Integer> storeId, int searchRadius) {
 		List<Store> result = new ArrayList<Store>();
-		for(int i = 0; i < storeId.size();i++) {
+		for (int i = 0; i < storeId.size(); i++) {
 			Store validStore = storeDao.searchStore(storeId.get(i));
-			if(validDistance(userCoordinates,validStore,searchRadius)) {
+			if (validDistance(userCoordinates, validStore, searchRadius)) {
 				result.add(validStore);
 			}
 		}
 		return result;
 	}
 
-	private boolean validDistance(String[] userCoordinates, Store validStore,int searchRadius) {
+	private boolean validDistance(String[] userCoordinates, Store validStore, int searchRadius) {
 		double lat1 = Double.parseDouble(userCoordinates[0]);
-		double lat2 = Double.parseDouble(validStore.getStore_latitude());;
-		double lng1 = Double.parseDouble(userCoordinates[1]);;
-		double lng2 = Double.parseDouble(validStore.getStore_longitude());;
-		double distance = distance(lat1,lat2,lng1,lng2,0.0,0.0);
+		double lat2 = Double.parseDouble(validStore.getStore_latitude());
+		;
+		double lng1 = Double.parseDouble(userCoordinates[1]);
+		;
+		double lng2 = Double.parseDouble(validStore.getStore_longitude());
+		;
+		double distance = distance(lat1, lat2, lng1, lng2, 0.0, 0.0);
 		return distance <= searchRadius;
 	}
-	
-	private double distance(double lat1, double lat2, double lon1,
-	        double lon2, double el1, double el2) {
 
-	    final int R = 6371; // Radius of the earth
+	private double distance(double lat1, double lat2, double lon1, double lon2, double el1, double el2) {
 
-	    double latDistance = Math.toRadians(lat2 - lat1);
-	    double lonDistance = Math.toRadians(lon2 - lon1);
-	    double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-	            + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-	            * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-	    double distance = R * c * 1000; // convert to meters
+		final int R = 6371; // Radius of the earth
 
-	    double height = el1 - el2;
+		double latDistance = Math.toRadians(lat2 - lat1);
+		double lonDistance = Math.toRadians(lon2 - lon1);
+		double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + Math.cos(Math.toRadians(lat1))
+				* Math.cos(Math.toRadians(lat2)) * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		double distance = R * c * 1000; // convert to meters
 
-	    distance = Math.pow(distance, 2) + Math.pow(height, 2);
+		double height = el1 - el2;
 
-	    return Math.sqrt(distance);
+		distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+		return Math.sqrt(distance);
 	}
 }
